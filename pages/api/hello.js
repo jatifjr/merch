2fc77bcffae8fe3e5467f20d6938cba34cc0c707
Ahmad Fajar Kusumajati <90365537+jatifjr@users.noreply.github.com>
2022-12-27 14:02:05 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,5 +3,6 @@ import { client } from "../../lib/client";
 export default async function handler(req, res) {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
+  console.log(products);
   res.status(200).json(products);
 }
