00ef245de92f703c1edac0fc5193404e2e9671c0
Ahmad Fajar Kusumajati <90365537+jatifjr@users.noreply.github.com>
2022-12-27 13:59:04 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { client } from "../../lib/client";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  res.status(200).json(products);
 }
